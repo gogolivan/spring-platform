7cb5d28245b6f4d175102d7c60b4911eb3d7ede9
Ivan Gogol <>
2025-07-01 21:50:16 +0200

    feat: add Notification document

---
+++ notification/src/main/java/com/github/gogolivan/springplatform/notification/model/Notification.java
@@ -0,0 +1,7 @@
+package com.github.gogolivan.springplatform.notification.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("NOTIFICATION")
+public class Notification {
+}
